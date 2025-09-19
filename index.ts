7075cb75b08c17706bd16a1a730b1ae86dc595bb
Denis Hadzipasic <denishadzipasic.webdev@gmail.com>
2025-09-19 21:53:37 +0200

    fixed deployment issue on compiled index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
-import "./db.ts";
+import "./db.js";
 
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import recipeRouter from "./routes/recipe-routes";
-import { errorHandler } from "./middlewares/errorHandler";
-import userRouter from "./routes/user-routes";
-import ingredientRouter from "./routes/ingredients-route";
+import recipeRouter from "./routes/recipe-routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import userRouter from "./routes/user-routes.js";
+import ingredientRouter from "./routes/ingredients-route.js";
 import path from "path";
 
 const app = express();
