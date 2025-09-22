b7b3aeaf1f11e421a6db88b617e4fe224fc19ed6
Denis Hadzipasic <denishadzipasic.webdev@gmail.com>
2025-09-22 20:07:24 +0200

    deployment with different client path

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,20 @@ import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const clientPath = path.join(process.cwd(), "client", "dist");
 
 app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, "client", "dist")));
+app.use(express.static(clientPath));
 
 app.use("/api/ingredient", ingredientRouter);
 app.use("/api/recipe", recipeRouter);
 app.use("/api/user", userRouter);
 
 app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(clientPath, "index.html"));
 });
 
 app.use(errorHandler);
